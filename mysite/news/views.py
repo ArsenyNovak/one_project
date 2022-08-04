a7b9b826f7335d3bbcee7aba87525b6e887bdb57
Arseny Novak <arsenya.11@mail.ru>
2022-08-04 16:34:37 +0300

    lesson_24_13:55

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import News, Category
+from django.shortcuts import render, get_object_or_404
 
+from .models import News, Category
+from .forms import NewsForm
 
 def index(request):
     news = News.objects.all()
@@ -14,4 +14,17 @@ def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
     category = Category.objects.get(pk=category_id)
     return render(request, 'news/category.html', {'news': news, 'category': category})
-# Create your views here.
+
+def view_news(request, news_id):
+ #   news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {"news_item": news_item})
+
+def add_news(request):
+    if request.method == 'POST':
+
+        pass
+    else:
+        form = NewsForm()
+        pass
+    return render(request, 'news/add_news.html', {'form':form})
